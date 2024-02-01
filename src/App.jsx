fd0fa126ca0c4c7d8d48bc2138678204bac11e63
Mohammad Ashif <Mohammad Ashif>
2024-02-01 13:40:22 +0600

    learning useRef

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useRef, useState } from 'react'
+import './App.css'
+
+function App() {
+  const myRef = useRef(null);
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    setCount(count+1);
+    myRef.current++;
+    console.log('ref:', myRef.current);
+  }
+
+  return (
+    <>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrement} style={{backgroundColor: "#0C2D57", color: "#fff"}}>Increment</button>
+    </>
+  )
+}
+
+export default App
